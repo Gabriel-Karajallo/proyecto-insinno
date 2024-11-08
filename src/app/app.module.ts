6ee7ebdb44a0a6b910d54a514f08fa5846888684
Gabriel <c.gabrielkarajallo@gmail.com>
2024-11-08 19:09:00 +0100

    #

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from '../../../heroes-app/src/app/material/material.module';
+import { HttpClientModule } from '@angular/common/http';
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -9,7 +12,10 @@ import { ProductsComponent } from './pages/products/products.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+
+
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +25,14 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     ProductsComponent,
     RegisterComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    Error404PageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    HttpClientModule,
   ],
   providers: [
     provideAnimationsAsync()
