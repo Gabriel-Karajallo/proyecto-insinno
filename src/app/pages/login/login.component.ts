ba043057c1d90721b851380500b73b67c9ab81fa
Gabriel-Karajallo <c.gabrielkarajallo@gmail.com>
2024-11-13 10:14:01 +0100

    #

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent implements OnInit{
 
