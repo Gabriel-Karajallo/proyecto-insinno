cc0ceee10794f4b08e4b1f4fd03b58e51b317a9d
Gabriel-Karajallo <c.gabrielkarajallo@gmail.com>
2024-11-22 13:53:18 +0100

    #

--- src/app/pages/dashboard/components/header/header.component.ts
+++ src/app/pages/dashboard/components/header/header.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  sidebarVisible: boolean = false;
 
+  items: MenuItem[] | undefined;
+
+ ngOnInit() {
+        this.items = [
+            {
+                separator: true
+            },
+            {
+                label: 'Productos',
+                items: [
+                    {
+                        label: 'Conciertos',
+                        icon: 'pi pi-plus',
+                    },
+                    {
+                        label: 'Festivales',
+                        icon: 'pi pi-search',
+                    },
+                    {
+                      label: 'Contacto',
+                      icon: 'pi pi-search',
+                  }
+                ]
+            },
+            {
+                label: 'Perfil',
+                items: [
+                    {
+                        label: 'Configuración',
+                        icon: 'pi pi-cog',
+                    },
+                    {
+                        label: 'Cerrar sesión',
+                        icon: 'pi pi-sign-out',
+                    }
+                ]
+            },
+            {
+                separator: true
+            }
+        ];
+    }
 }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -9,6 +9,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 
+
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { BadgeModule } from 'primeng/badge';
+import { RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+
 @NgModule({
   declarations: [
     DashboardComponent,
@@ -20,6 +28,15 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     CommonModule,
     DashboardRoutingModule,
+
+
+    //PRIME NG MODULES
+    SidebarModule,
+    ButtonModule,
+    MenuModule,
+    BadgeModule,
+    RippleModule,
+    AvatarModule
   ],
 
 
