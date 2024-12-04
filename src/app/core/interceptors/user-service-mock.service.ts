a0f1eb11e1bae8db1edf4ecfc26be8a6bac43488
Gabriel-Karajallo <c.gabrielkarajallo@gmail.com>
2024-12-04 08:36:32 +0100

    #

--- src/app/core/interceptors/user-service-mock.service.ts
+++ src/app/core/interceptors/user-service-mock.service.ts
@@ -2,55 +2,31 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } fr
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { environments } from '../../environment/environment';
-import { ProfileComponent } from '../../pages/profile/profile.component';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceMockService implements HttpInterceptor {
-
-
-
-
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (environments.isMock) {
-      // Simula la respuesta al iniciar sesión
-      if (req.url.endsWith('auth/login') && req.method === 'POST') {
-        return of(new HttpResponse({
-          status: 200,
-          body: {
-            jwt: 'token123456', // Token simulado
-            id: 1,
-            name: 'John',
-            email: 'johndoe@example.com',
-            password: '123456A',
-          }
-        }));
-      }
 
-      // Simula una solicitud protegida con token (ejemplo: GET /users/1)
-      if (req.url.endsWith('api/users/') && req.method === 'GET') {
+    if (!environments.isMock) {
+      return next.handle(req);
+    }
 
-        // Si el token es correcto, devuelve los datos simulados
-        return of(new HttpResponse({
-          status: 200,
-          body: {
-            id: 1,
-            name: 'John',
-            email: 'johndoe@example.com',
-            password: '123456A',
-          }
-        }));
-      }
-      if (req.url.endsWith('/users/delete') && req.method === 'DELETE') {
-        return of(new HttpResponse({
-          status: 200,
-          body: { message: 'Mock: Usuario eliminado correctamente' }
-        }));
-      }
+    // Simula la respuesta al iniciar sesión
+    if (req.url.endsWith('auth/login') && req.method === 'POST') {
+      return of(new HttpResponse({
+        status: 200,
+        body: {
+          jwt: 'token123456', // Token simulado
+          id: 1,
+          username: 'John',
+          email: 'johndoe@example.com',
+          password: '123456A',
+        }
+      }));
     }
     return next.handle(req);
-
   }
 }
